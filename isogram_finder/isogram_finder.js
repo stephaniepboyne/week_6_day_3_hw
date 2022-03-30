55d32fd00b202e661c9139883e20c4301e6b527a
stephaniepboyne <stephanie_boyne@yahoo.co.uk>
2022-03-30 20:27:06 +0100

    isogram done

--- isogram_finder/isogram_finder.js
+++ isogram_finder/isogram_finder.js
@@ -1,9 +1,17 @@
 const IsogramFinder = function (word) {
-
+    this.word = word.toLowerCase();
 }
 
 IsogramFinder.prototype.isIsogram = function () {
-
+    newWord = this.word.split('');
+    return newWord.every((letter) => {
+        return newWord.indexOf(letter) === this.word.lastIndexOf(letter);
+    });
 }
+
+
+
+
+
 
 module.exports = IsogramFinder;
